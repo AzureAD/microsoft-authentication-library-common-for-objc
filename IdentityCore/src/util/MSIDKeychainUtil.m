4a74810cfdda148dcf48c2d2f22f2978647c957c
Sergey Demchenko <antrix1989@gmail.com>
2017-11-28 20:11:21 -0800

    Add MSIDKeychainUtil.

---
+++ IdentityCore/src/util/MSIDKeychainUtil.m
@@ -0,0 +1,79 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "MSIDKeychainUtil.h"
+
+@implementation MSIDKeychainUtil
+
+#pragma mark - Public
+
++ (NSString *)teamId
+{
+    static dispatch_once_t s_once;
+    static NSString *s_keychainTeamId = nil;
+    
+    dispatch_once(&s_once, ^{
+        s_keychainTeamId = [self retrieveTeamIDFromKeychain];
+        
+        MSID_LOG_INFO(nil, @"Using \"%@\" Team ID for Keychain.", _PII_NULLIFY(s_keychainTeamId));
+        MSID_LOG_INFO_PII(nil, @"Using \"%@\" Team ID for Keychain.", s_keychainTeamId);
+    });
+    
+    return s_keychainTeamId;
+}
+
+#pragma mark - Private
+
++ (NSString *)retrieveTeamIDFromKeychain
+{
+    NSDictionary *query = @{ (id)kSecClass : (id)kSecClassGenericPassword,
+                             (id)kSecAttrAccount : @"teamIDHint",
+                             (id)kSecAttrService : @"",
+                             (id)kSecReturnAttributes : @YES };
+    CFDictionaryRef result = nil;
+    
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&result);
+    
+    if (status == errSecItemNotFound)
+    {
+        NSMutableDictionary* addQuery = [query mutableCopy];
+        [addQuery setObject:(id)kSecAttrAccessibleAlways forKey:(id)kSecAttrAccessible];
+        status = SecItemAdd((__bridge CFDictionaryRef)addQuery, (CFTypeRef *)&result);
+    }
+    
+    if (status != errSecSuccess)
+    {
+        MSID_LOG_ERROR(nil, @"fetching team ID, status: %d", status);
+        return nil;
+    }
+    
+    NSString *accessGroup = [(__bridge NSDictionary *)result objectForKey:(__bridge id)(kSecAttrAccessGroup)];
+    NSArray *components = [accessGroup componentsSeparatedByString:@"."];
+    NSString *bundleSeedID = [components firstObject];
+    
+    CFRelease(result);
+    
+    return [bundleSeedID length] ? bundleSeedID : nil;
+}
+
+@end
