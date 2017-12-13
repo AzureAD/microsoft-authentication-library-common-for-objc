ca9d089a1036cddb1174fba72a8d3a84f6a4c5be
Yong Zeng <zeyong@microsoft.com>
2017-12-12 16:30:01 -0800

    add correlationId to MSIDError

--- IdentityCore/src/MSIDError.h
+++ IdentityCore/src/MSIDError.h
@@ -24,6 +24,7 @@
 extern NSString *MSIDErrorDescriptionKey;
 extern NSString *MSIDOAuthErrorKey;
 extern NSString *MSIDOAuthSubErrorKey;
+extern NSString *MSIDCorrelationIdKey;
 extern NSString *MSIDHTTPHeadersKey;
 
 /*!
@@ -40,5 +41,5 @@ typedef NS_ENUM(NSInteger, MSIDErrorCode)
     removeMeWhenThingsAreAdded = -10000,
 };
 
-extern NSError *MSIDCreateError(NSString *domain, NSInteger code, NSString *errorDescription, NSString *oauthError, NSString *subError, NSError *underlyingError, NSDictionary *additionalUserInfo);
+extern NSError *MSIDCreateError(NSString *domain, NSInteger code, NSString *errorDescription, NSString *oauthError, NSString *subError, NSError *underlyingError, NSUUID *correlationId, NSDictionary *additionalUserInfo);
 
--- IdentityCore/src/MSIDError.m
+++ IdentityCore/src/MSIDError.m
@@ -24,17 +24,19 @@
 NSString *MSIDErrorDescriptionKey = @"MSIDErrorDescriptionKey";
 NSString *MSIDOAuthErrorKey = @"MSIDOAuthErrorKey";
 NSString *MSIDOAuthSubErrorKey = @"MSIDOAuthSubErrorKey";
+NSString *MSIDCorrelationIdKey = @"MSIDCorrelationIdKey";
 NSString *MSIDHTTPHeadersKey = @"MSIDHTTPHeadersKey";
 
 NSString *MSIDErrorDomain = @"MSIDErrorDomain";
 
-NSError *MSIDCreateError(NSString *domain, NSInteger code, NSString *errorDescription, NSString *oauthError, NSString *subError, NSError *underlyingError, NSDictionary *additionalUserInfo)
+NSError *MSIDCreateError(NSString *domain, NSInteger code, NSString *errorDescription, NSString *oauthError, NSString *subError, NSError *underlyingError, NSUUID *correlationId, NSDictionary *additionalUserInfo)
 {
     NSMutableDictionary *userInfo = [NSMutableDictionary new];
     userInfo[MSIDErrorDescriptionKey] = errorDescription;
     userInfo[MSIDOAuthErrorKey] = oauthError;
     userInfo[MSIDOAuthSubErrorKey] = subError;
     userInfo[NSUnderlyingErrorKey]  = underlyingError;
+    userInfo[MSIDCorrelationIdKey] = correlationId;
     if (additionalUserInfo)
     {
         [userInfo addEntriesFromDictionary:additionalUserInfo];
