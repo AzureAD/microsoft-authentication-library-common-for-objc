69068c2a1f3961523e67d28e809d01c62ed8fc54
Olga Dalton <oldalton@microsoft.com>
2017-12-14 17:29:12 -0800

    Fix failing tests

--- IdentityCore/tests/MSIDKeyedArchiverSerializerTests.m
+++ IdentityCore/tests/MSIDKeyedArchiverSerializerTests.m
@@ -24,6 +24,7 @@
 #import <XCTest/XCTest.h>
 #import "MSIDKeyedArchiverSerializer.h"
 #import "MSIDToken.h"
+#import "NSDictionary+MSIDTestUtil.h"
 
 @interface MSIDKeyedArchiverSerializerTests : XCTestCase
 
