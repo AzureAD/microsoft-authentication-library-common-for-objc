a8a6dd2e587c05c8af7354dd6f840fc259c17f6d
Jason Zeng <zeyong@microsoft.com>
2018-03-08 17:30:37 -0800

    Add unit tests

---
+++ IdentityCore/tests/integration/ios/MSIDWipeDataTelemetryTests.m
@@ -0,0 +1,134 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <XCTest/XCTest.h>
+#import "MSIDTelemetryTestDispatcher.h"
+#import "MSIDTelemetry.h"
+#import "MSIDTokenCacheItem.h"
+#import "MSIDLegacyTokenCacheAccessor.h"
+#import "MSIDKeychainTokenCache.h"
+#import "MSIDSharedTokenCache.h"
+#import "MSIDAccount.h"
+#import "MSIDTestCacheIdentifiers.h"
+#import "MSIDRefreshToken.h"
+#import "MSIDTestTokenResponse.h"
+#import "MSIDTestRequestParams.h"
+#import "MSIDAADV1TokenResponse.h"
+#import "MSIDTelemetry+Internal.h"
+#import "MSIDTelemetryEventStrings.h"
+
+@interface MSIDTestRequestContext : NSObject <MSIDRequestContext>
+
+@property (retain, nonatomic) NSUUID* correlationId;
+@property (retain, nonatomic) NSString* telemetryRequestId;
+@property (retain, nonatomic) NSString* logComponent;
+
+@end
+
+@implementation MSIDTestRequestContext
+@end
+
+
+@interface MSIDWipeDataTelemetryTests : XCTestCase
+{
+    MSIDKeychainTokenCache *_dataSource;
+    MSIDLegacyTokenCacheAccessor *_cacheAccessor;
+}
+
+@end
+
+@implementation MSIDWipeDataTelemetryTests
+
+- (void)setUp
+{
+    _dataSource = [[MSIDKeychainTokenCache alloc] init];
+    _cacheAccessor = [[MSIDLegacyTokenCacheAccessor alloc] initWithDataSource:_dataSource];
+    
+    [super setUp];
+}
+
+- (void)testWipeDataTelemetry_whenGetTokenButNone_shouldLogWipeDataInTelemetry
+{
+    // setup telemetry callback
+    MSIDTelemetryTestDispatcher *dispatcher = [MSIDTelemetryTestDispatcher new];
+    
+    NSMutableArray *receivedEvents = [NSMutableArray array];
+    
+    // the dispatcher will store the telemetry events it receives
+    [dispatcher setTestCallback:^(id<MSIDTelemetryEventInterface> event)
+     {
+         [receivedEvents addObject:event];
+     }];
+    
+    // register the dispatcher
+    [[MSIDTelemetry sharedInstance] addDispatcher:dispatcher];
+    
+    // save a refresh token to keychain token cache
+    MSIDRefreshToken *token = [[MSIDRefreshToken alloc] initWithTokenResponse:[MSIDTestTokenResponse v1DefaultTokenResponse]
+                                                                      request:[MSIDTestRequestParams v1DefaultParams]];
+    MSIDAccount *account = [[MSIDAccount alloc] initWithLegacyUserId:DEFAULT_TEST_ID_TOKEN_USERNAME
+                                                        uniqueUserId:nil];
+    MSIDTestRequestContext *reqContext = [MSIDTestRequestContext new];
+    [reqContext setTelemetryRequestId:[[MSIDTelemetry sharedInstance] generateRequestId]];
+    NSError *error = nil;
+    
+    BOOL result = [_cacheAccessor saveRefreshToken:token
+                                            account:account
+                                            context:reqContext
+                                              error:&error];
+    
+    // remove the refresh token to trigger wipe data being written
+    result = [_cacheAccessor removeToken:token
+                                  account:account
+                                  context:reqContext
+                                    error:&error];
+    
+    // read the refresh token in order to log wipe data in telemetry
+    MSIDBaseToken *returnedToken = [_cacheAccessor getTokenWithType:MSIDTokenTypeRefreshToken
+                                                             account:account
+                                                       requestParams:[MSIDTestRequestParams v1DefaultParams]
+                                                             context:reqContext
+                                                               error:&error];
+    
+    // expect no token because it has been deleted
+    XCTAssertNil(error);
+    XCTAssertNil(returnedToken);
+    
+    
+    
+    // test if wipe data is logged in telemetry
+    for (id<MSIDTelemetryEventInterface> event in receivedEvents)
+    {
+        if ([event.propertyMap[@"Microsoft.Test.event_name"] isEqualToString:MSID_TELEMETRY_EVENT_TOKEN_CACHE_LOOKUP])
+        {
+            XCTAssertEqualObjects(event.propertyMap[@"Microsoft.Test.wipe_app"], @"com.microsoft.MSIDTestsHostApp");
+            XCTAssertNotNil(event.propertyMap[@"Microsoft.Test.wipe_time"]);
+            return;
+        }
+    }
+    
+    XCTAssertTrue(NO);
+}
+
+@end
+
