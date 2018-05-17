c79d2e7f270d0b533026bd32aa09b188ad8ba106
Jason Zeng <zeyong@microsoft.com>
2018-05-17 15:20:08 -0700

    Bring over the fix from release/2.6.4 branch

--- IdentityCore/src/webview/embeddedWebview/challangeHandlers/MSIDNegotiateHandler.m
+++ IdentityCore/src/webview/embeddedWebview/challangeHandlers/MSIDNegotiateHandler.m
@@ -23,6 +23,7 @@
 
 #import "MSIDNegotiateHandler.h"
 #import "MSIDChallengeHandler.h"
+#include <GSS/GSS.h>
 
 @implementation MSIDNegotiateHandler
 
@@ -38,10 +39,109 @@
                 context:(id<MSIDRequestContext>)context
       completionHandler:(ChallengeCompletionHandler)completionHandler
 {
-    // This challenge is rejected and the next authentication protection space should be tried by OS
-    completionHandler(NSURLSessionAuthChallengeRejectProtectionSpace, nil);
+#pragma unused(challenge)
+    
+    if ([self hasValidKBRCredential:context])
+    {
+        // This means we have an unexpired credential, let system handle it
+        MSID_LOG_INFO(context, @"Perform default system handling for Negotiate");
+        completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+    }
+    else
+    {
+        // This challenge is rejected and the next authentication protection space should be tried by OS
+        MSID_LOG_INFO(context, @"Reject protection space, so the next one can be tried");
+        completionHandler(NSURLSessionAuthChallengeRejectProtectionSpace, nil);
+    }
     
     return YES;
+}
+
+#pragma mark - Credential checking
+
++ (BOOL)hasValidKBRCredential:(id<MSIDRequestContext>)context
+{
+    OM_uint32 minor;
+    
+    __block BOOL foundUnexpiredKBRCredential = NO;
+    
+    MSID_LOG_INFO(context, @"Checking credentials to handle Negotiate challenge");
+    
+    // Go over all credentials that can be found
+    gss_iter_creds(&minor, 0, GSS_KRB5_MECHANISM,
+                   ^(gss_iter_OID mechOid, gss_cred_id_t credential) {
+                       
+                       (void)mechOid;
+                       
+                       if (credential == GSS_C_NO_CREDENTIAL)
+                       {
+                           MSID_LOG_INFO(context, @"No more credentials found for GSS_KRB5_MECHANISM");
+                           return;
+                       }
+                       
+                       MSID_LOG_INFO(context, @"Found a credential, now check its validity...");
+                       
+                       // Copy the name describing the credential
+                       gss_name_t credentialName = GSSCredentialCopyName(credential);
+                       
+                       if (credentialName != GSS_C_NO_NAME)
+                       {
+                           CFStringRef displayableName = GSSNameCreateDisplayString(credentialName);
+                           
+                           // Get the lifetime of this credential
+                           uint32_t lifeTime = GSSCredentialGetLifetime(credential);
+                           
+                           MSID_LOG_INFO(context, @"Found credential for GSS_KRB5_MECHANISM with lifetime %d", lifeTime);
+                           MSID_LOG_INFO_PII(context, @"Found credential for GSS_KRB5_MECHANISM with lifetime %d, displayable name %@", lifeTime, displayableName);
+                           
+                           if (displayableName)
+                           {
+                               CFRelease(displayableName);
+                           }
+                           
+                           // Found an unexpired credential
+                           if (lifeTime > 0)
+                           {
+                               MSID_LOG_INFO(context, @"Found unexpired credential");
+                               
+                               foundUnexpiredKBRCredential = YES;
+                               releaseCredential(&credential);
+                               releaseName(&credentialName);
+                               return;
+                           }
+                       }
+                       else
+                       {
+                           MSID_LOG_INFO_PII(context, @"Failed to get credential name, skip");
+                       }
+                       
+                       releaseName(&credentialName);
+                       releaseCredential(&credential);
+                   });
+    
+    return foundUnexpiredKBRCredential;
+}
+
+#pragma mark - Helpers
+
+static void releaseCredential(gss_cred_id_t *credential)
+{
+    if (credential && *credential != GSS_C_NO_CREDENTIAL)
+    {
+        OM_uint32 minorStatus;
+        gss_release_cred(&minorStatus, credential);
+        *credential = GSS_C_NO_CREDENTIAL;
+    }
+}
+
+static void releaseName(gss_name_t *name)
+{
+    if (name && *name != GSS_C_NO_NAME)
+    {
+        OM_uint32 minorStatus;
+        gss_release_name(&minorStatus, name);
+        *name = GSS_C_NO_NAME;
+    }
 }
 
 @end
