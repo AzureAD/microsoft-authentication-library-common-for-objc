94a7622937f81a8b2d72ff31e4698d62c5ad6f46
Sergey Demchenko <sedemche@microsoft.com>
2018-09-07 14:53:00 -0700

    Provide underlyingError in MSIDAuthorityFactory during authority parsing.

--- IdentityCore/src/validation/MSIDAuthorityFactory.m
+++ IdentityCore/src/validation/MSIDAuthorityFactory.m
@@ -40,27 +40,28 @@
                             context:(id<MSIDRequestContext>)context
                               error:(NSError **)error
 {
+    NSError *underlyingError;
     if ([MSIDB2CAuthority isAuthorityFormatValid:url context:context error:nil])
     {
-        __auto_type b2cAuthority = [[MSIDB2CAuthority alloc] initWithURL:url context:context error:nil];
+        __auto_type b2cAuthority = [[MSIDB2CAuthority alloc] initWithURL:url context:context error:&underlyingError];
         if (b2cAuthority) return b2cAuthority;
     }
     
     if ([MSIDADFSAuthority isAuthorityFormatValid:url context:context error:nil])
     {
-        __auto_type adfsAuthority = [[MSIDADFSAuthority alloc] initWithURL:url context:context error:nil];
+        __auto_type adfsAuthority = [[MSIDADFSAuthority alloc] initWithURL:url context:context error:&underlyingError];
         if (adfsAuthority) return adfsAuthority;
     }
     
     if ([MSIDAADAuthority isAuthorityFormatValid:url context:context error:nil])
     {
-        __auto_type aadAuthority = [[MSIDAADAuthority alloc] initWithURL:url rawTenant:rawTenant context:context error:nil];
+        __auto_type aadAuthority = [[MSIDAADAuthority alloc] initWithURL:url rawTenant:rawTenant context:context error:&underlyingError];
         if (aadAuthority) return aadAuthority;
     }
     
     if (error)
     {
-        *error = MSIDCreateError(MSIDErrorDomain, MSIDErrorInvalidDeveloperParameter, @"Provided authority url is not a valid authority.", nil, nil, nil, context.correlationId, nil);
+        *error = MSIDCreateError(MSIDErrorDomain, MSIDErrorInvalidDeveloperParameter, @"Provided authority url is not a valid authority.", nil, nil, underlyingError, context.correlationId, nil);
         
         MSID_LOG_ERROR(context, @"Provided authority url is not a valid authority.");
     }
