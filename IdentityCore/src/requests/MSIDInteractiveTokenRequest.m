6af118b28b00562243587c0c282379a6340ec24c
Olga Dalton <oldalton@microsoft.com>
2018-11-17 18:50:33 -0800

    Updated comments

--- IdentityCore/src/requests/MSIDInteractiveTokenRequest.m
+++ IdentityCore/src/requests/MSIDInteractiveTokenRequest.m
@@ -99,7 +99,7 @@
         /*
 
          TODO: this code has been moved from MSAL almost as is to avoid too many changes in this logic.
-         Some minor refactoring will be done separately: https://github.com/AzureAD/microsoft-authentication-library-common-for-objc/issues/297
+         Some minor refactoring to this logic and to the tests will be done separately: https://github.com/AzureAD/microsoft-authentication-library-common-for-objc/issues/297
          */
 
         if ([response isKindOfClass:MSIDWebOAuth2Response.class])
