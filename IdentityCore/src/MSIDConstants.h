c4ee1d068aa502fd9b3e834cd31df84ffececd78
Olga Dalton <oldalton@microsoft.com>
2018-11-28 20:30:12 -0800

    Added default authenticator redirect uri

--- IdentityCore/src/MSIDConstants.h
+++ IdentityCore/src/MSIDConstants.h
@@ -58,6 +58,7 @@ extern NSString * _Nonnull const MSID_BROKER_ADAL_SCHEME;
 extern NSString * _Nonnull const MSID_BROKER_MSAL_SCHEME;
 extern NSString * _Nonnull const MSID_BROKER_APP_BUNDLE_ID;
 extern NSString * _Nonnull const MSID_BROKER_APP_BUNDLE_ID_DF;
+extern NSString * _Nonnull const MSID_AUTHENTICATOR_REDIRECT_URI;
 
 extern NSString * _Nonnull const MSIDTrustedAuthority;
 extern NSString * _Nonnull const MSIDTrustedAuthorityUS;
