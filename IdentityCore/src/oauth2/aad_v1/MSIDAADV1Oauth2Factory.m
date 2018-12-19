a5d5a4e4829b7e0b19fb693c80a247099f3b769b
Mike Della Donna <midella@microsoft.com>
2018-12-19 12:56:48 -0500

    Missed one constant, re-labled others in the wrong order

--- IdentityCore/src/oauth2/aad_v1/MSIDAADV1Oauth2Factory.m
+++ IdentityCore/src/oauth2/aad_v1/MSIDAADV1Oauth2Factory.m
@@ -118,7 +118,7 @@
             {
                 errorCode = MSIDErrorServerProtectionPoliciesRequired;
                 userInfo = [[NSMutableDictionary alloc] initWithCapacity:1];
-                userInfo[MSIDUserDisplayableIdKey] = response.jsonDictionary[MSID_ADDITIONAL_USER_IDENTIFIER];
+                userInfo[MSIDUserDisplayableIdKey] = response.jsonDictionary[MSID_USER_DISPLAYABLE_IDENTIFIER];
             }
 
             if (error)
