871a826a3ceb786e6006f1b49a35e81ff6b079a7
Jason Zeng <zeyong@microsoft.com>
2019-01-22 18:59:08 -0800

    Add UTs

--- IdentityCore/tests/MSIDCredentialCacheItemTests.m
+++ IdentityCore/tests/MSIDCredentialCacheItemTests.m
@@ -240,4 +240,50 @@
     cacheItem2.clientId = DEFAULT_TEST_CLIENT_ID;
     XCTAssertNotEqualObjects(cacheItem1, cacheItem2);
 }
+
+- (void)testStoreAppKey_whenNilKeyString_shouldNotStore
+{
+    MSIDCredentialCacheItem *cacheItem = [MSIDCredentialCacheItem new];
+    
+    [cacheItem storeAppKey:nil];
+    
+    XCTAssertNil(cacheItem.appKey);
+}
+
+- (void)testStoreAppKey_whenEmptyKeyString_shouldNotStore
+{
+    MSIDCredentialCacheItem *cacheItem = [MSIDCredentialCacheItem new];
+    
+    [cacheItem storeAppKey:@""];
+    
+    XCTAssertNil(cacheItem.appKey);
+}
+
+- (void)testStoreAppKey_whenInvalidKeyStringWithoutDelimiter_shouldNotStore
+{
+    MSIDCredentialCacheItem *cacheItem = [MSIDCredentialCacheItem new];
+    
+    [cacheItem storeAppKey:@"abc"];
+    
+    XCTAssertNil(cacheItem.appKey);
+}
+
+- (void)testStoreAppKey_whenInvalidKeyStringWithTooManyDelimiters_shouldNotStore
+{
+    MSIDCredentialCacheItem *cacheItem = [MSIDCredentialCacheItem new];
+    
+    [cacheItem storeAppKey:@"abc|d|e"];
+    
+    XCTAssertNil(cacheItem.appKey);
+}
+
+- (void)testStoreAppKey_whenValidKeyString_shouldStore
+{
+    MSIDCredentialCacheItem *cacheItem = [MSIDCredentialCacheItem new];
+    
+    [cacheItem storeAppKey:@"abc|d"];
+    
+    XCTAssertEqualObjects(cacheItem.appKey, @"d");
+}
+
 @end
