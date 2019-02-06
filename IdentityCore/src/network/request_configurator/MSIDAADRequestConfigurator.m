9c34a7c2c392d2cc0bd69b4b19d50ab1a3364a0c
Jason Zeng <zeyong@microsoft.com>
2019-02-06 13:43:24 -0800

    Disable PKeyAuth support for Mac

--- IdentityCore/src/network/request_configurator/MSIDAADRequestConfigurator.m
+++ IdentityCore/src/network/request_configurator/MSIDAADRequestConfigurator.m
@@ -77,7 +77,9 @@ static NSTimeInterval const s_defaultTimeoutInterval = 300;
     mutableUrlRequest.URL = requestUrl;
     mutableUrlRequest.timeoutInterval = self.timeoutInterval;
     mutableUrlRequest.cachePolicy = NSURLRequestReloadIgnoringCacheData;
+#if TARGET_OS_IPHONE
     [mutableUrlRequest setValue:kMSIDPKeyAuthHeaderVersion forHTTPHeaderField:kMSIDPKeyAuthHeader];
+#endif
     [mutableUrlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
     
     NSMutableDictionary *headers = [mutableUrlRequest.allHTTPHeaderFields mutableCopy];
