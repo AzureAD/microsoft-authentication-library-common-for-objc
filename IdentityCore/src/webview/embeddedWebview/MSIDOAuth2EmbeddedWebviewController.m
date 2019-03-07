eb0e1b31e38e863e1bef84ba9226b750bbe3a73f
Olga Dalton <oldalton@microsoft.com>
2019-03-06 17:49:47 -0800

    Removed TODo

--- IdentityCore/src/webview/embeddedWebview/MSIDOAuth2EmbeddedWebviewController.m
+++ IdentityCore/src/webview/embeddedWebview/MSIDOAuth2EmbeddedWebviewController.m
@@ -303,7 +303,6 @@
 {
     __auto_type isKnown = [MSIDAADNetworkConfiguration.defaultConfiguration isAADPublicCloud:endURL.host];
     MSID_LOG_VERBOSE(self.context, @"-completeWebAuthWithURL: %@", isKnown ? endURL.host : @"unknown host");
-    // TODO: remove me!
     MSID_LOG_VERBOSE_PII(self.context, @"-completeWebAuthWithURL: %@", [endURL msidPIINullifiedURL]);
     
     [self endWebAuthWithURL:endURL error:nil];
