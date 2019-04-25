059a34191d0132e0e59d7c241f60134718d15cd6
Alan Chan <alanch@microsoft.com>
2019-04-25 14:07:32 -0700

    [WIP] Command line test tool compiles and runs

---
+++ IdentityCore/MSIDTestKeychainUtil/main.m
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "MSIDBasicContext.h"
+#import "MSIDAccountCacheItem.h"
+#import "MSIDAccountCredentialCache.h"
+#import "MSIDCacheItemJsonSerializer.h"
+#import "MSIDCacheKey.h"
+#import "MSIDClientInfo.h"
+#import "MSIDDefaultAccountCacheKey.h"
+#import "MSIDDefaultAccountCacheQuery.h"
+#import "MSIDMacKeychainTokenCache.h"
+#import "MSIDTestIdentifiers.h"
+#import "NSDictionary+MSIDTestUtil.h"
+#import "NSString+MSIDExtensions.h"
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        MSIDMacKeychainTokenCache* dataSource = [MSIDMacKeychainTokenCache new];
+        MSIDAccountCredentialCache* cache = [[MSIDAccountCredentialCache alloc] initWithDataSource:dataSource];
+        MSIDCacheItemJsonSerializer* serializer = [MSIDCacheItemJsonSerializer new];
+        if (cache && serializer)
+            printf("good\n");
+    }
+    return 0;
+}
