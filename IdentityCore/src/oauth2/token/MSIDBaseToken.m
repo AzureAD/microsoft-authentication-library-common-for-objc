48f5fcce5b40768ad3cf726be0febb00a7e5e3ce
Dean Collins <decoll@microsoft.com>
2019-05-03 11:25:25 -0700

    more additionalInfo removal

--- IdentityCore/src/oauth2/token/MSIDBaseToken.m
+++ IdentityCore/src/oauth2/token/MSIDBaseToken.m
@@ -143,7 +143,10 @@
             return nil;
         }
         
-        _additionalServerInfo = tokenCacheItem.additionalInfo;
+        if (tokenCacheItem.speInfo)
+        {
+            _additionalServerInfo = @{MSID_SPE_INFO_CACHE_KEY: tokenCacheItem.speInfo};
+        }
 
         if (tokenCacheItem.homeAccountId)
         {
@@ -170,7 +173,7 @@
 
     cacheItem.realm = self.authority.url.msidTenant;
     cacheItem.clientId = self.clientId;
-    cacheItem.additionalInfo = self.additionalServerInfo;
+    cacheItem.speInfo = self.additionalServerInfo[MSID_SPE_INFO_CACHE_KEY];
     cacheItem.homeAccountId = self.accountIdentifier.homeAccountId;
     return cacheItem;
 }
