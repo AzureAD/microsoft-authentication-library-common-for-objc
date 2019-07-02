2c329338b9b7aedf9b8334fd0c332a0199910e84
Olga Dalton <oldalton@microsoft.com>
2019-07-01 17:06:53 -0700

    Don't return AT if ID is missing

--- IdentityCore/src/requests/MSIDSilentTokenRequest.m
+++ IdentityCore/src/requests/MSIDSilentTokenRequest.m
@@ -147,15 +147,14 @@
             MSIDTokenResult *tokenResult = [self resultWithAccessToken:accessToken
                                                           refreshToken:refreshableToken
                                                                  error:&resultError];
-
-            if (resultError)
+            
+            if (tokenResult)
             {
-                completionBlock(nil, resultError);
+                completionBlock(tokenResult, nil);
                 return;
             }
-
-            completionBlock(tokenResult, nil);
-            return;
+            
+            MSID_LOG_WARN_PII(nil, @"Couldn't create result for cached access token, error %@. Try to recover...", resultError);
         }
 
         if (accessToken && accessToken.isExtendedLifetimeValid)
