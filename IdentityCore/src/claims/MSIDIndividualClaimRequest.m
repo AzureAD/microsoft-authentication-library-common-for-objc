5808f1b08415966af1a6ed8cce15c013013c48b1
Sergey Demchenko <sedemche@microsoft.com>
2019-09-27 14:47:21 -0700

    Modify error code for claims json parsing.

--- IdentityCore/src/claims/MSIDIndividualClaimRequest.m
+++ IdentityCore/src/claims/MSIDIndividualClaimRequest.m
@@ -75,7 +75,7 @@
         
         if (![json[claimName] isKindOfClass:NSNull.class])
         {
-            if (![json msidAssertType:NSDictionary.class ofKey:claimName required:YES error:error]) return nil;
+            if (![json msidAssertTypeIsOneOf:@[NSDictionary.class] ofKey:claimName required:YES context:nil errorCode:MSIDErrorInvalidDeveloperParameter error:error]) return nil;
             
             NSError *localError;
             __auto_type additinalInfo = [[MSIDIndividualClaimRequestAdditionalInfo alloc] initWithJSONDictionary:json[claimName] error:&localError];
