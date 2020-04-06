9adee2bb13a5de3e44182cf7d77f849b8176816c
Olga Dalton <oldalton@microsoft.com>
2020-04-05 17:06:16 -0700

    Annotate class correctly

--- IdentityCore/src/requests/broker/MSIDSSOExtensionRequestDelegate+Internal.h
+++ IdentityCore/src/requests/broker/MSIDSSOExtensionRequestDelegate+Internal.h
@@ -28,6 +28,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+API_AVAILABLE(ios(13.0), macos(10.15))
 @interface MSIDSSOExtensionRequestDelegate ()
 
 @property (nonatomic, readonly) MSIDJsonSerializer *jsonSerializer;
