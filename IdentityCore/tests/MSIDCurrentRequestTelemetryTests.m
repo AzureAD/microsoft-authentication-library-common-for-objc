80da42a25c6acd67da920068860fe8c377fc8280
mipetriu <mipetriu@microsoft.com>
2020-05-06 13:21:05 -0700

    Fix formatting of headers on unit test methods

--- IdentityCore/tests/MSIDCurrentRequestTelemetryTests.m
+++ IdentityCore/tests/MSIDCurrentRequestTelemetryTests.m
@@ -1,5 +1,3 @@
-//------------------------------------------------------------------------------
-//
 // Copyright (c) Microsoft Corporation.
 // All rights reserved.
 //
@@ -22,8 +20,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-//
-//------------------------------------------------------------------------------
 
 #import <XCTest/XCTest.h>
 #import "MSIDCurrentRequestTelemetry.h"
