bb435c37b2146a5f556e5f6faaa47f1f066fc13c
Olga Dalton <oldalton@microsoft.com>
2020-05-12 17:27:57 -0700

    Added draft common interface for assymetric key generation

---
+++ IdentityCore/src/cache/crypto/MSIDAssymetricKeyKeychainGenerator.m
@@ -0,0 +1,247 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "MSIDAssymetricKeyKeychainGenerator.h"
+#import "MSIDKeychainUtil.h"
+#import "MSIDAssymetricKeyPair.h"
+#import "MSIDAssymetricKeyLookupAttributes.h"
+
+@interface MSIDAssymetricKeyKeychainGenerator()
+
+@property (nonatomic) NSString *keychainGroup;
+@property (nonatomic) NSDictionary *defaultKeychainQuery;
+
+@end
+
+@implementation MSIDAssymetricKeyKeychainGenerator
+
+#pragma mark - Init
+
+- (nullable instancetype)initWithGroup:(nullable NSString *)keychainGroup error:(NSError * _Nullable __autoreleasing * _Nullable)error
+{
+    if (!(self = [super init]))
+    {
+        return nil;
+    }
+    
+    if (!keychainGroup)
+    {
+        keychainGroup = [[NSBundle mainBundle] bundleIdentifier];
+    }
+    
+    MSIDKeychainUtil *keychainUtil = [MSIDKeychainUtil sharedInstance];
+    if (!keychainUtil.teamId)
+    {
+        if (error)
+        {
+            *error = MSIDCreateError(MSIDErrorDomain, MSIDErrorInternal, @"Failed to retrieve teamId from keychain.", nil, nil, nil, nil, nil, YES);
+        }
+        
+        MSID_LOG_WITH_CTX(MSIDLogLevelError, nil, @"Failed to retrieve teamId from keychain.");
+        return nil;
+    }
+    
+    // Add team prefix to keychain group if it is missed.
+    if (![keychainGroup hasPrefix:keychainUtil.teamId])
+    {
+        keychainGroup = [keychainUtil accessGroup:keychainGroup];
+    }
+    
+    _keychainGroup = keychainGroup;
+    
+    if (!_keychainGroup)
+    {
+        if (error)
+        {
+            *error = MSIDCreateError(MSIDErrorDomain, MSIDErrorInternal, @"Failed to set keychain access group.", nil, nil, nil, nil, nil, YES);
+        }
+        
+        MSID_LOG_WITH_CTX(MSIDLogLevelError, nil, @"Failed to set keychain access group.");
+        return nil;
+    }
+    
+    NSMutableDictionary *defaultKeychainQuery = [@{(id)kSecAttrAccessGroup : self.keychainGroup} mutableCopy];
+    
+#ifdef __MAC_OS_X_VERSION_MAX_ALLOWED
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 101500
+    if (@available(macOS 10.15, *)) {
+        defaultKeychainQuery[(id)kSecUseDataProtectionKeychain] = @YES;
+    }
+#endif
+#endif
+    
+    self.defaultKeychainQuery = defaultKeychainQuery;
+    return self;
+}
+
+#pragma mark - MSIDAssymetricKeyGenerating
+
+- (MSIDAssymetricKeyPair *)generateKeyPairForAttributes:(MSIDAssymetricKeyLookupAttributes *)attributes
+                                                  error:(NSError **)error
+{
+    // 0. Cleanup any previous state
+    BOOL cleanupResult = [self cleanupKeychainForAttributes:attributes error:error];
+    
+    if (!cleanupResult)
+    {
+        [self logAndFillError:@"Failed to cleanup keychain prior to generating new keypair. Proceeding might result in unexpected results. Keychain might need to be manually cleaned to recover." status:-1 error:error];
+        return nil;
+    }
+    
+    // 1. Generate keypair
+    NSDictionary *keyPairAttr = [self keychainQueryWithAttributes:[attributes assymetricKeyPairAttributes]];
+    
+    OSStatus status = SecKeyGeneratePair((__bridge CFDictionaryRef)keyPairAttr, NULL, NULL);
+    
+    if (status != errSecSuccess)
+    {
+        [self logAndFillError:@"Failed to generate keypair" status:status error:error];
+        return nil;
+    }
+    
+    // 2. Return keys
+    return [self getKeyPairForAttributes:attributes error:error];
+}
+
+- (MSIDAssymetricKeyPair *)readOrGenerateKeyPairForAttributes:(MSIDAssymetricKeyLookupAttributes *)attributes
+                                                        error:(NSError **)error
+{
+    NSError *readError = nil;
+    MSIDAssymetricKeyPair *keyPair = [self getKeyPairForAttributes:attributes error:&readError];
+    
+    if (keyPair || readError)
+    {
+        if (error) *error = readError;
+        return keyPair;
+    }
+    
+    return [self generateKeyPairForAttributes:attributes error:error];
+}
+
+- (MSIDAssymetricKeyPair *)getKeyPairForAttributes:(MSIDAssymetricKeyLookupAttributes *)attributes
+                                             error:(NSError **)error
+{
+    NSDictionary *privateKeyAttributes = [self keyAttributesWithQueryDictionary:[attributes privateKeyAttributes] keyTitle:@"private key" error:error];
+    
+    if (!privateKeyAttributes)
+    {
+        return nil;
+    }
+    
+    NSDictionary *publicKeyAttributes = [self keyAttributesWithQueryDictionary:[attributes publicKeyAttributes] keyTitle:@"public key" error:error];
+    
+    if (!publicKeyAttributes)
+    {
+        return nil;
+    }
+    
+    SecKeyRef privateKeyRef = (__bridge SecKeyRef)privateKeyAttributes[(__bridge id)kSecValueRef];
+    SecKeyRef publicKeyRef = (__bridge SecKeyRef)publicKeyAttributes[(__bridge id)kSecValueRef];
+    
+    if (!privateKeyRef || !publicKeyRef)
+    {
+        [self logAndFillError:@"Invalid keychain attributes. No key ref returned" status:-1 error:error];
+        return nil;
+    }
+    
+    MSIDAssymetricKeyPair *keypair = [[MSIDAssymetricKeyPair alloc] initWithPrivateKey:privateKeyRef
+                                                                             publicKey:publicKeyRef];
+
+    
+    return keypair;
+}
+
+#pragma mark - Cleanup
+
+- (BOOL)cleanupKeychainForAttributes:(MSIDAssymetricKeyLookupAttributes *)attributes error:(NSError **)error
+{
+    return [self deleteItemWithAttributes:[attributes privateKeyAttributes] itemTitle:@"private key" error:error]
+        && [self deleteItemWithAttributes:[attributes publicKeyAttributes] itemTitle:@"public key" error:error];
+}
+
+- (BOOL)deleteItemWithAttributes:(NSDictionary *)attributes itemTitle:(NSString *)itemTitle error:(NSError **)error
+{
+    NSDictionary *queryAttributes = [self keychainQueryWithAttributes:attributes];
+    OSStatus result = SecItemDelete((CFDictionaryRef)queryAttributes);
+    
+    if (result != errSecSuccess
+        && result != errSecItemNotFound)
+    {
+        [self logAndFillError:[NSString stringWithFormat:@"Failed to remove %@", itemTitle]
+                       status:result
+                        error:error];
+        return NO;
+    }
+    
+    return YES;
+}
+
+#pragma mark - Private
+
+- (NSDictionary *)keyAttributesWithQueryDictionary:(NSDictionary *)queryDictionary keyTitle:(NSString *)keyTitle error:(NSError **)error
+{
+    NSMutableDictionary *keychainQuery = [[self keychainQueryWithAttributes:queryDictionary] mutableCopy];
+    keychainQuery[(__bridge id)kSecReturnAttributes] = @YES;
+    keychainQuery[(__bridge id)kSecReturnRef] = @YES;
+    
+    CFTypeRef keyCFDict = NULL;
+    
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)keychainQuery, (CFTypeRef*)&keyCFDict);
+    
+    if (status != errSecSuccess)
+    {
+        if (status != errSecItemNotFound)
+        {
+            [self logAndFillError:[NSString stringWithFormat:@"Failed to find %@", keyTitle]
+                           status:status
+                            error:error];
+        }
+        
+        return nil;
+    }
+    
+    NSDictionary *privateKeyDict = CFBridgingRelease(keyCFDict);
+    return privateKeyDict;
+}
+
+- (NSDictionary *)keychainQueryWithAttributes:(NSDictionary *)attributes
+{
+    NSMutableDictionary *keyPairAttr = [self.defaultKeychainQuery mutableCopy];
+    [keyPairAttr addEntriesFromDictionary:attributes];
+    return keyPairAttr;
+}
+
+#pragma mark - Utils
+
+- (void)logAndFillError:(NSString *)errorTitle status:(OSStatus)status error:(NSError **)error
+{
+    NSString *description = [NSString stringWithFormat:@"Operation failed with title \"%@\", status %ld", errorTitle, (long)status];
+    MSID_LOG_WITH_CTX(MSIDLogLevelError, nil, @"%@", description);
+    
+    if (error)
+    {
+        *error = MSIDCreateError(MSIDErrorDomain, MSIDErrorInternal, description, nil, nil, nil, nil, nil, NO);
+    }
+}
+
+@end
