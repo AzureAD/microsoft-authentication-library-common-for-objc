6f6f413d3e9525ce03574fc62bb9821a708aa421
Olga Dalton <oldalton@microsoft.com>
2020-07-14 21:57:55 -0700

    Revert CBA URL change

--- IdentityCore/src/webview/embeddedWebview/challangeHandlers/ios/MSIDCertAuthHandler.m
+++ IdentityCore/src/webview/embeddedWebview/challangeHandlers/ios/MSIDCertAuthHandler.m
@@ -134,14 +134,7 @@ static BOOL s_useAuthSession = NO;
         // This will launch a Safari view within the current Application, removing the app flip. Our control of this
         // view is extremely limited. Safari is still running in a separate sandbox almost completely isolated from us.
         
-        NSURL *currentURL = requestURL;
-        
-        if (s_useAuthSession && webview.URL)
-        {
-            currentURL = webview.URL;
-        }
-        
-        s_systemWebViewController = [[MSIDSystemWebviewController alloc] initWithStartURL:currentURL
+        s_systemWebViewController = [[MSIDSystemWebviewController alloc] initWithStartURL:requestURL
                                                                               redirectURI:redirectURI
                                                                          parentController:parentViewController
                                                                  useAuthenticationSession:s_useAuthSession
