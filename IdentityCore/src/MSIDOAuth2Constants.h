cd14fcfda13a7671abdd183ac0323467274e39f7
Olga Dalton <oldalton@microsoft.com>
2020-07-26 10:46:10 -0700

    Store PRT expires in time

--- IdentityCore/src/MSIDOAuth2Constants.h
+++ IdentityCore/src/MSIDOAuth2Constants.h
@@ -109,6 +109,7 @@ extern NSString *const MSID_EXPIRES_ON_CACHE_KEY;
 extern NSString *const MSID_OAUTH_TOKEN_TYPE_CACHE_KEY;
 extern NSString *const MSID_CACHED_AT_CACHE_KEY;
 extern NSString *const MSID_EXTENDED_EXPIRES_ON_CACHE_KEY;
+extern NSString *const MSID_EXPIRES_IN_CACHE_KEY;
 extern NSString *const MSID_SPE_INFO_CACHE_KEY;
 extern NSString *const MSID_RESOURCE_RT_CACHE_KEY;
 extern NSString *const MSID_LOCAL_ACCOUNT_ID_CACHE_KEY;
