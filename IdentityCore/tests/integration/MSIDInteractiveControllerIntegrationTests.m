49778ad5b154e6bad6b06d1afed67ac00afe7ef4
kaisong1990 <kaisong1990@gmail.com>
2020-10-02 08:39:49 -0700

    rebased and put wait time as 3s

--- IdentityCore/tests/integration/MSIDInteractiveControllerIntegrationTests.m
+++ IdentityCore/tests/integration/MSIDInteractiveControllerIntegrationTests.m
@@ -351,7 +351,7 @@
         [expectation fulfill];
     }];
 
-    [self waitForExpectationsWithTimeout:10.0 handler:nil];
+    [self waitForExpectationsWithTimeout:3.0 handler:nil];
 }
 
 - (void)testAcquireToken_whenWPJRequest_shouldReturnWorkplaceJoinRequiredError
