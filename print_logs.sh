c63b9dd54c342f6f8c102461ab651e26d911a2b0
Ameya Patil <amepatil@microsoft.com>
2021-06-29 17:59:58 -0400

    Shifting status.txxt to git ignored build folder. Adding test report tab to pipeline. Removing print_logs.sh since we're adding junit test report

--- print_logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in ~/Library/Developer/Xcode/DerivedData/IdentityCore-*/Logs/Test/Diagnostics/IdentityCoreTests-*/*; do
-	bname=$(basename $f)
-	echo "##[group]$bname"
-	cat $f
-	echo "##[endgroup]$bname"
-done
