1b72b5e28578351ea6ca75d0ac715d87a511b0fa
Olga Dalton <olgadalton@Olgas-MacBook-Pro.local>
2021-07-02 18:16:07 -0700

    Fixed macro

--- IdentityCore/src/util/ASAuthorizationSingleSignOnProvider+MSIDExtensions.m
+++ IdentityCore/src/util/ASAuthorizationSingleSignOnProvider+MSIDExtensions.m
@@ -65,8 +65,8 @@
 }
 
 + (void)setRequiresUI:(BOOL)requiresUI forRequest:(ASAuthorizationSingleSignOnRequest *)ssoRequest
-{
-#if __IPHONEOS_VERSION_MAX_ALLOWED >= 150000 || __MAC_OS_X_VERSION_MAX_ALLOWED >= 120000
+{    
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_15_0 || __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_12_0
     if (@available(iOS 15.0, macOS 12.0, *))
     {
         ssoRequest.userInterfaceEnabled = requiresUI;
