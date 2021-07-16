84aac647a538880af3b251de1586b1f52e126622
Olga Dalton <olgadalton@Olgas-MacBook-Pro.local>
2021-07-15 20:51:05 -0700

    Added error handling and tests

--- IdentityCore/src/util/ASAuthorizationSingleSignOnProvider+MSIDExtensions.m
+++ IdentityCore/src/util/ASAuthorizationSingleSignOnProvider+MSIDExtensions.m
@@ -66,7 +66,7 @@
 
 + (void)setRequiresUI:(BOOL)requiresUI forRequest:(ASAuthorizationSingleSignOnRequest *)ssoRequest
 {    
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_15_0 || __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_12_0
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 150000 || __MAC_OS_X_VERSION_MAX_ALLOWED >= 120000
     if (@available(iOS 15.0, macOS 12.0, *))
     {
         ssoRequest.userInterfaceEnabled = requiresUI;
