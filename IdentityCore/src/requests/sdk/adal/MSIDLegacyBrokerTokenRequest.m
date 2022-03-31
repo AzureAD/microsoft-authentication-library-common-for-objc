43d36fa2506eccc01168187d36e303827b09009a
Alexey Ursulenko <alexeyu@microsoft.com>
2022-03-30 21:45:23 -0700

    exclude legacy broker for msalcpp

--- IdentityCore/src/requests/sdk/adal/MSIDLegacyBrokerTokenRequest.m
+++ IdentityCore/src/requests/sdk/adal/MSIDLegacyBrokerTokenRequest.m
@@ -21,6 +21,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#if !EXCLUDE_FROM_MSALCPP
+
 #import "MSIDLegacyBrokerTokenRequest.h"
 #import "MSIDInteractiveTokenRequestParameters.h"
 #import "MSIDAccountIdentifier.h"
@@ -77,3 +79,5 @@
 }
 
 @end
+
+#endif
