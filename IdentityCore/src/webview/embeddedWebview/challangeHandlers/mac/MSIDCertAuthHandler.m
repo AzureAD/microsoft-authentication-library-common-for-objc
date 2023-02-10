ca6135083c0ebf3c92aaf78b05e0d1ad92c311c5
Fidelia Nawar <fidelianawar@Fidelias-MacBook-Pro-2.local>
2023-02-10 14:29:34 -0600

    memorizing certificate choice for preferred identity macos

--- IdentityCore/src/webview/embeddedWebview/challangeHandlers/mac/MSIDCertAuthHandler.m
+++ IdentityCore/src/webview/embeddedWebview/challangeHandlers/mac/MSIDCertAuthHandler.m
@@ -78,7 +78,8 @@
              }
             
             // If there is no preferred identity saved, we must set preferred identity certificate using hostname and key usage parameters
-            if (identity == NO){
+            if (identity == NULL)
+            {
                 NSArray *arr = @[(__bridge NSString *)kSecAttrCanSign,(__bridge NSString *)kSecAttrCanEncrypt,(__bridge NSString *)kSecAttrCanDecrypt];
                 CFArrayRef arrayRef = (__bridge CFArrayRef)arr;
                 OSStatus status = SecIdentitySetPreferred(selectedIdentity, (CFStringRef)host, arrayRef);
