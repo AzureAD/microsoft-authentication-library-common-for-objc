ea08db0d7a3bb5ccfbb79f60af332c6b21679adb
Kai <kaisong1990@gmail.com>
2023-02-27 10:27:49 -0800

    Update IdentityCore/src/util/NSError+MSIDThrottlingExtension.h
    Co-authored-by: Juan Arias <juan-arias@users.noreply.github.com>

--- IdentityCore/src/util/NSError+MSIDThrottlingExtension.h
+++ IdentityCore/src/util/NSError+MSIDThrottlingExtension.h
@@ -35,5 +35,5 @@ NS_ASSUME_NONNULL_BEGIN
 NS_ASSUME_NONNULL_END
 
 - (NSString *_Nullable)msidGetUserInfoValueWithMSIDKey:(NSString * _Nonnull)msidKey
-                                    orMSALKey:(NSString * _Nonnull)msalKey;
+                                             orMSALKey:(NSString * _Nonnull)msalKey;
 @end
