983df958a259ccc4589b296d7d8b8b2059f8678a
Olga Dalton <oldalton@microsoft.com>
2023-03-16 12:33:46 -0700

    Addressed comments

--- IdentityCore/src/oauth2/token/MSIDPrimaryRefreshToken.h
+++ IdentityCore/src/oauth2/token/MSIDPrimaryRefreshToken.h
@@ -27,7 +27,7 @@
 typedef NS_ENUM(NSInteger, MSIDExternalPRTKeyLocationType)
 {
     MSIDExternalPRTKeyLocationTypeNone = 0, // Key isn't stored externally
-    MSIDExternalPRTKeyLocationTypeWPJ = 1, // WPJ device and STK keys stored in secure enclave are used for PRT operations (for device-bound cases)
+    MSIDExternalPRTKeyLocationTypeWPJSecureEnclave = 1, // WPJ device and STK keys stored in secure enclave are used for PRT operations (for device-bound cases)
     MSIDExternalPRTKeyLocationTypeSSO = 2, // External SSO keys are used for PRT operations
     MSIDExternalPRTKeyLocationTypeSecureEnclave = 3 // External secure enclave keys are used for PRT operations (for deviceless cases)
 };
