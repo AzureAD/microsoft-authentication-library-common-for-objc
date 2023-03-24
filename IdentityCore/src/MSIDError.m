8b8b7d51c77405a396e4af90bbe3b73897fc9802
Swasti Gupta <swastigupta@Swastis-MacBook-Pro.local>
2023-03-24 01:03:13 -0700

    Added MSID Error code strings

--- IdentityCore/src/MSIDError.m
+++ IdentityCore/src/MSIDError.m
@@ -218,7 +218,7 @@ void MSIDFillAndLogError(NSError **error, MSIDErrorCode errorCode, NSString *err
     MSID_LOG_WITH_CORR_PII(MSIDLogLevelError, correlationID, @"Encountered error with code %ld, description %@", (long)errorCode, MSID_PII_LOG_MASKABLE(errorDescription));
 }
 
-NSString* MSIDErrorCodeString(MSIDErrorCode errorCode)
+NSString *MSIDErrorCodeToString(MSIDErrorCode errorCode)
 {
     switch (errorCode)
     {
