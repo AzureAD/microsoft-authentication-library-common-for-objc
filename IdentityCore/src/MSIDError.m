8699cf2d2d3a1bb3b031495144a720c511bac5d1
Swasti Gupta <swastigupta@Swastis-MacBook-Pro.local>
2023-04-07 19:47:58 -0700

    add error

--- IdentityCore/src/MSIDError.m
+++ IdentityCore/src/MSIDError.m
@@ -103,7 +103,7 @@ MSIDErrorCode MSIDErrorCodeForOAuthError(NSString *oauthError, MSIDErrorCode def
     return defaultCode;
 }
 
-NSDictionary* MSIDErrorDomainsAndCodes()
+NSDictionary* MSIDErrorDomainsAndCodes(void)
 {
     return @{ MSIDErrorDomain : @[// General Errors
                       @(MSIDErrorInternal),
