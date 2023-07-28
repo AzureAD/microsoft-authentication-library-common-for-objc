44de3738d79feeb74720586219b962dc9c5c4246
Sergey Demchenko <sedemche@microsoft.com>
2023-07-27 17:57:30 -0700

    Filter expired certs in the picker.

--- IdentityCore/src/webview/embeddedWebview/challangeHandlers/mac/MSIDCertAuthHandler.m
+++ IdentityCore/src/webview/embeddedWebview/challangeHandlers/mac/MSIDCertAuthHandler.m
@@ -23,6 +23,8 @@
 
 #import "MSIDCertAuthHandler.h"
 #import "MSIDCertificateChooser.h"
+#import "NSDate+MSIDExtensions.h"
+#import "MSIDKeychainUtil.h"
 
 @implementation MSIDCertAuthHandler
 
@@ -50,9 +52,12 @@
         identity = SecIdentityCopyPreferred((CFStringRef)webview.URL.absoluteString, NULL, (CFArrayRef)distinguishedNames);
     }
     
-    if (identity != NULL)
+    BOOL isIdentityValid = [self isIdentityValid:identity context:context];
+    
+    if (isIdentityValid)
     {
         MSID_LOG_WITH_CTX(MSIDLogLevelInfo, context, @"Using preferred identity");
+        
         [self respondCertAuthChallengeWithIdentity:identity context:context completionHandler:completionHandler];
     }
     else
@@ -120,6 +125,7 @@
        (id)kSecClass : (id)kSecClassIdentity,
        (id)kSecReturnRef: @YES,
        (id)kSecMatchLimit : (id)kSecMatchLimitAll,
+       (id)kSecMatchValidOnDate: (id)kCFNull // Pass a value of kCFNull to indicate the current date.
        } mutableCopy];
     
     if (issuers.count > 0)
@@ -127,24 +133,105 @@
         [query setObject:issuers forKey:(id)kSecMatchIssuers];
     }
     
-    CFTypeRef result = NULL;
-    
-    OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, &result);
-    if (status == errSecItemNotFound)
-    {
-        MSID_LOG_WITH_CORR(MSIDLogLevelInfo, correlationId, @"No certificate found matching challenge");
-        completionHandler(nil);
-        return;
-    }
-    else if (status != errSecSuccess)
-    {
-        MSID_LOG_WITH_CORR(MSIDLogLevelError, correlationId, @"Failed to find identity matching issuers with %d error.", status);
-        completionHandler(nil);
-        return;
-    }
-    
-    [MSIDCertificateChooserHelper showCertSelectionSheet:(__bridge NSArray *)result host:host webview:webview correlationId:correlationId completionHandler:completionHandler];
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+        CFTypeRef result = NULL;
+        // This is heavy operation, call it on the bg thread so we don't block UI.
+        OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, &result);
+        
+        // Proceed on the main queue: either show the cert picker or return immediately in case of error.
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (status == errSecItemNotFound)
+            {
+                MSID_LOG_WITH_CORR(MSIDLogLevelInfo, correlationId, @"No certificate found matching challenge");
+                completionHandler(nil);
+                return;
+            }
+            else if (status != errSecSuccess)
+            {
+                MSID_LOG_WITH_CORR(MSIDLogLevelError, correlationId, @"Failed to find identity matching issuers with %d error.", status);
+                completionHandler(nil);
+                return;
+            }
+            
+            [MSIDCertificateChooserHelper showCertSelectionSheet:(__bridge NSArray *)result host:host webview:webview correlationId:correlationId completionHandler:completionHandler];
+        });
+    });
 }
 
+#pragma mark - Private
+
++ (BOOL)isIdentityValid:(SecIdentityRef)identity context:(id<MSIDRequestContext>)context
+{
+    MSID_LOG_WITH_CTX(MSIDLogLevelInfo, context, @"Validating identity...");
+    if (identity == NULL)
+    {
+        MSID_LOG_WITH_CTX(MSIDLogLevelInfo, context, @"Identity validation finished: identity is NULL.");
+        return NO;
+    }
+    
+    BOOL result = NO;
+    
+    SecCertificateRef certificateRef = NULL;
+    OSStatus status = SecIdentityCopyCertificate(identity, &certificateRef); // +1
+    
+    if (certificateRef)
+    {
+        result = [self isCertificatedValid:certificateRef context:context];
+        
+        CFReleaseNull(certificateRef);
+    }
+    else
+    {
+        MSID_LOG_WITH_CTX(MSIDLogLevelError, context, @"Failed to copy certificate from identityref with status %d", (int)status);
+    }
+    
+    MSID_LOG_WITH_CTX(MSIDLogLevelInfo, context, @"Identity validation finished, result: %d", result);
+    
+    return result;
+}
+
++ (BOOL)isCertificatedValid:(SecCertificateRef)certificate context:(id<MSIDRequestContext>)context
+{
+    NSDate *notBeforeDate = [self dateFromCertificate:certificate key:kSecOIDX509V1ValidityNotBefore context:context];
+    NSDate *notAfterDate = [self dateFromCertificate:certificate key:kSecOIDX509V1ValidityNotAfter context:context];
+    
+    NSDate *nowDate = [NSDate new];
+    return [nowDate msidIsDateBetween:notBeforeDate dateAfter:notAfterDate];
+}
+
++ (NSDate *)dateFromCertificate:(SecCertificateRef)certificate key:(CFTypeRef)dateKey context:(id<MSIDRequestContext>)context
+{
+    const void *keys[] = {dateKey};
+    
+    CFErrorRef cfError;
+    CFArrayRef keySelection = CFArrayCreate(NULL, keys, sizeof(keys)/sizeof(keys[0]), &kCFTypeArrayCallBacks);
+    CFDictionaryRef dict = SecCertificateCopyValues(certificate, keySelection, &cfError);
+    CFRelease(keySelection);
+    
+    if (dict == NULL)
+    {
+        NSError *error = (__bridge NSError *)cfError;
+        MSID_LOG_WITH_CTX(MSIDLogLevelError, context, @"Failed to read date form cert with error %@", error);
+
+        return NULL;
+    }
+    
+    CFDictionaryRef metadata = CFDictionaryGetValue(dict, dateKey);
+    CFNumberRef value = metadata ? CFDictionaryGetValue(metadata, kSecPropertyKeyValue) : NULL;
+    
+    NSDate *date;
+    if (value != NULL)
+    {
+        CFAbsoluteTime at;
+        if (CFNumberGetValue(value, kCFNumberDoubleType, &at))
+        {
+            date = [NSDate dateWithTimeIntervalSinceReferenceDate:(NSTimeInterval)at];
+        }
+    }
+    
+    CFRelease(dict);
+    
+    return date;
+}
 
 @end
