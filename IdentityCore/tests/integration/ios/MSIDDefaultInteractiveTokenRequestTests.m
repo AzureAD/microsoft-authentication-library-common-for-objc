864bae1f788949c595c1784acd9532460496a3db
mipetriu <mipetriu@microsoft.com>
2024-04-02 13:53:35 -0700

    revert commented out test

--- IdentityCore/tests/integration/ios/MSIDDefaultInteractiveTokenRequestTests.m
+++ IdentityCore/tests/integration/ios/MSIDDefaultInteractiveTokenRequestTests.m
@@ -841,79 +841,79 @@
     [self waitForExpectationsWithTimeout:1 handler:nil];
 }
 
-#if TARGET_OS_IPHONE
-//- (void)testInteractiveRequestFlow_whenOpenBrowserResponse_shouldOpenLink
-//{
-//    __block NSUUID *correlationId = [NSUUID new];
-//
-//    MSIDInteractiveTokenRequestParameters *parameters = [MSIDInteractiveTokenRequestParameters new];
-//    parameters.target = @"fakescope1 fakescope2";
-//    parameters.authority = [@"https://login.microsoftonline.com/common" aadAuthority];
-//    parameters.redirectUri = @"x-msauth-test://com.microsoft.testapp";
-//    parameters.clientId = @"my_client_id";
-//    parameters.extraAuthorizeURLQueryParameters = @{ @"eqp1" : @"val1", @"eqp2" : @"val2" };
-//    parameters.loginHint = @"fakeuser@contoso.com";
-//    parameters.correlationId = correlationId;
-//    parameters.webviewType = MSIDWebviewTypeWKWebView;
-//    parameters.extraScopesToConsent = @"fakescope3";
-//    parameters.oidcScope = @"openid profile offline_access";
-//    parameters.promptType = MSIDPromptTypeConsent;
-//    parameters.authority.openIdConfigurationEndpoint = [NSURL URLWithString:@"https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"];
-//    parameters.accountIdentifier = [[MSIDAccountIdentifier alloc] initWithDisplayableId:@"user@contoso.com" homeAccountId:@"1.1234-5678-90abcdefg"];
-//    parameters.enablePkce = YES;
-//
-//    MSIDInteractiveTokenRequest *request = [[MSIDInteractiveTokenRequest alloc] initWithRequestParameters:parameters oauthFactory:[MSIDAADV2Oauth2Factory new] tokenResponseValidator:[MSIDDefaultTokenResponseValidator new] tokenCache:self.tokenCache accountMetadataCache:self.metadataCache extendedTokenCache:nil];
-//
-//    XCTAssertNotNil(request);
-//
-//    // Swizzle out the main entry point for WebUI, WebUI is tested in its own component tests
-//    [MSIDTestSwizzle classMethod:@selector(startSessionWithWebView:oauth2Factory:configuration:context:completionHandler:)
-//                           class:[MSIDWebviewAuthorization class]
-//                           block:(id)^(
-//                               __unused id obj,
-//                               __unused NSObject<MSIDWebviewInteracting> *webview,
-//                               __unused MSIDOauth2Factory *oauth2Factory,
-//                               __unused MSIDBaseWebRequestConfiguration *configuration,
-//                               __unused id<MSIDRequestContext> context,
-//                               MSIDWebviewAuthCompletionHandler completionHandler)
-//    {
-//
-//         NSString *responseString = @"browser://login.microsoftonline.appinstall.test";
-//
-//         MSIDWebOpenBrowserResponse *msauthResponse = [[MSIDWebOpenBrowserResponse alloc] initWithURL:[NSURL URLWithString:responseString] context:nil error:nil];
-//         completionHandler(msauthResponse, nil);
-//     }];
-//
-//    NSString *authority = @"https://login.microsoftonline.com/common";
-//    MSIDTestURLResponse *discoveryResponse = [MSIDTestURLResponse discoveryResponseForAuthority:authority];
-//    [MSIDTestURLSession addResponse:discoveryResponse];
-//
-//    MSIDTestURLResponse *oidcResponse = [MSIDTestURLResponse oidcResponseForAuthority:authority];
-//    [MSIDTestURLSession addResponse:oidcResponse];
-//
-//    XCTestExpectation *expectation = [self expectationWithDescription:@"Run request."];
-//
-//    [request executeRequestWithCompletion:^(MSIDTokenResult * _Nullable result, NSError * _Nullable error, MSIDWebWPJResponse * _Nullable installBrokerResponse) {
-//
-//        XCTAssertNil(result);
-//        XCTAssertNotNil(error);
-//        XCTAssertEqual(error.code, MSIDErrorSessionCanceledProgrammatically);
-//        XCTAssertNil(installBrokerResponse);
-//
-//        [expectation fulfill];
-//
-//    }];
-//
-//    XCTestExpectation *openURLExpectation = [self expectationWithDescription:@"Open URL."];
-//
-//    [MSIDApplicationTestUtil onOpenURL:^BOOL(NSURL *url, __unused NSDictionary<NSString *,id> *options) {
-//        XCTAssertEqualObjects(url.absoluteString, @"https://login.microsoftonline.appinstall.test");
-//        [openURLExpectation fulfill];
-//        return YES;
-//    }];
-//
-//    [self waitForExpectationsWithTimeout:1 handler:nil];
-//}
+#if TARGET_OS_IPHONE && !TARGET_OS_VISION
+- (void)testInteractiveRequestFlow_whenOpenBrowserResponse_shouldOpenLink
+{
+    __block NSUUID *correlationId = [NSUUID new];
+
+    MSIDInteractiveTokenRequestParameters *parameters = [MSIDInteractiveTokenRequestParameters new];
+    parameters.target = @"fakescope1 fakescope2";
+    parameters.authority = [@"https://login.microsoftonline.com/common" aadAuthority];
+    parameters.redirectUri = @"x-msauth-test://com.microsoft.testapp";
+    parameters.clientId = @"my_client_id";
+    parameters.extraAuthorizeURLQueryParameters = @{ @"eqp1" : @"val1", @"eqp2" : @"val2" };
+    parameters.loginHint = @"fakeuser@contoso.com";
+    parameters.correlationId = correlationId;
+    parameters.webviewType = MSIDWebviewTypeWKWebView;
+    parameters.extraScopesToConsent = @"fakescope3";
+    parameters.oidcScope = @"openid profile offline_access";
+    parameters.promptType = MSIDPromptTypeConsent;
+    parameters.authority.openIdConfigurationEndpoint = [NSURL URLWithString:@"https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"];
+    parameters.accountIdentifier = [[MSIDAccountIdentifier alloc] initWithDisplayableId:@"user@contoso.com" homeAccountId:@"1.1234-5678-90abcdefg"];
+    parameters.enablePkce = YES;
+
+    MSIDInteractiveTokenRequest *request = [[MSIDInteractiveTokenRequest alloc] initWithRequestParameters:parameters oauthFactory:[MSIDAADV2Oauth2Factory new] tokenResponseValidator:[MSIDDefaultTokenResponseValidator new] tokenCache:self.tokenCache accountMetadataCache:self.metadataCache extendedTokenCache:nil];
+
+    XCTAssertNotNil(request);
+
+    // Swizzle out the main entry point for WebUI, WebUI is tested in its own component tests
+    [MSIDTestSwizzle classMethod:@selector(startSessionWithWebView:oauth2Factory:configuration:context:completionHandler:)
+                           class:[MSIDWebviewAuthorization class]
+                           block:(id)^(
+                               __unused id obj,
+                               __unused NSObject<MSIDWebviewInteracting> *webview,
+                               __unused MSIDOauth2Factory *oauth2Factory,
+                               __unused MSIDBaseWebRequestConfiguration *configuration,
+                               __unused id<MSIDRequestContext> context,
+                               MSIDWebviewAuthCompletionHandler completionHandler)
+    {
+
+         NSString *responseString = @"browser://login.microsoftonline.appinstall.test";
+
+         MSIDWebOpenBrowserResponse *msauthResponse = [[MSIDWebOpenBrowserResponse alloc] initWithURL:[NSURL URLWithString:responseString] context:nil error:nil];
+         completionHandler(msauthResponse, nil);
+     }];
+
+    NSString *authority = @"https://login.microsoftonline.com/common";
+    MSIDTestURLResponse *discoveryResponse = [MSIDTestURLResponse discoveryResponseForAuthority:authority];
+    [MSIDTestURLSession addResponse:discoveryResponse];
+
+    MSIDTestURLResponse *oidcResponse = [MSIDTestURLResponse oidcResponseForAuthority:authority];
+    [MSIDTestURLSession addResponse:oidcResponse];
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Run request."];
+
+    [request executeRequestWithCompletion:^(MSIDTokenResult * _Nullable result, NSError * _Nullable error, MSIDWebWPJResponse * _Nullable installBrokerResponse) {
+
+        XCTAssertNil(result);
+        XCTAssertNotNil(error);
+        XCTAssertEqual(error.code, MSIDErrorSessionCanceledProgrammatically);
+        XCTAssertNil(installBrokerResponse);
+
+        [expectation fulfill];
+
+    }];
+
+    XCTestExpectation *openURLExpectation = [self expectationWithDescription:@"Open URL."];
+
+    [MSIDApplicationTestUtil onOpenURL:^BOOL(NSURL *url, __unused NSDictionary<NSString *,id> *options) {
+        XCTAssertEqualObjects(url.absoluteString, @"https://login.microsoftonline.appinstall.test");
+        [openURLExpectation fulfill];
+        return YES;
+    }];
+
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+}
 #endif
 
 @end
