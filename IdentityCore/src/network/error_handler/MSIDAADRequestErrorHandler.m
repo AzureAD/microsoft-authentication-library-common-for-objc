18f5dfd777a037938bdcb12b741d118b14c73a32
Kai Song <kai@Kais-MacBook-Pro.local>
2024-07-22 14:21:42 -0700

    Only do when should retry

--- IdentityCore/src/network/error_handler/MSIDAADRequestErrorHandler.m
+++ IdentityCore/src/network/error_handler/MSIDAADRequestErrorHandler.m
@@ -50,7 +50,7 @@
         {
             // Networking errors (-1001, -1003. -1004. -1005. -1009)
             shouldRetryNetworkingFailure = [self shouldRetryNetworkingFailure:error.code];
-            if (error.code == NSURLErrorNotConnectedToInternet)
+            if (shouldRetryNetworkingFailure && error.code == NSURLErrorNotConnectedToInternet)
             {
                 // For handling the NSURLErrorNotConnectedToInternet error, retry the network request after a longer delay.
                 httpRequest.retryInterval = 2.0;
