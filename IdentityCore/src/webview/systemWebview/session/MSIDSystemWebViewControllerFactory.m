001195928163f8a6f8e1ec283e67b2eef54241eb
Silviu Petrescu <spetrescu@microsoft.com>
2024-08-02 10:18:48 +0100

    PR comments

--- IdentityCore/src/webview/systemWebview/session/MSIDSystemWebViewControllerFactory.m
+++ IdentityCore/src/webview/systemWebview/session/MSIDSystemWebViewControllerFactory.m
@@ -39,10 +39,8 @@
     {
         return preferredType;
     }
-#if (TARGET_OS_IPHONE || TARGET_OS_MAC) && !TARGET_OS_MACCATALYST
+
     return MSIDWebviewTypeAuthenticationSession;
-#endif
-    return MSIDWebviewTypeWKWebView;
 }
 
 + (id<MSIDWebviewInteracting>)authSessionWithParentController:(__unused MSIDViewController *)parentController
