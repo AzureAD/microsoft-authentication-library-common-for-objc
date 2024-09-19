6b7e17c0e032fbb78a4f6174e4ba246bcfcfdf55
Olga Dalton <olgadalton@Olgas-MacBook-Pro.local>
2024-09-19 14:11:57 -0700

    Revert PPE change

--- IdentityCore/src/MSIDAADNetworkConfiguration.m
+++ IdentityCore/src/MSIDAADNetworkConfiguration.m
@@ -44,7 +44,7 @@ static NSSet<NSString *> *s_trustedHostList;
                              MSIDTrustedAuthorityGermany,
                              MSIDTrustedAuthorityWorldWide,
                              MSIDTrustedAuthorityUSGovernment,
-                             MSIDTrustedAuthorityCloudGovApi, @"login.windows-ppe.net", nil];
+                             MSIDTrustedAuthorityCloudGovApi, nil];
     }
 }
 
