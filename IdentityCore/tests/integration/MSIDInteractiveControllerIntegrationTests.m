c83dc6496f98157abe16f44673342059204210ea
Hieu Nguyen <hieunguyen@Hieus-Mac-24.local>
2024-09-20 12:02:53 -0700

    init

--- IdentityCore/tests/integration/MSIDInteractiveControllerIntegrationTests.m
+++ IdentityCore/tests/integration/MSIDInteractiveControllerIntegrationTests.m
@@ -353,7 +353,7 @@
         [expectation fulfill];
     }];
 
-    [self waitForExpectationsWithTimeout:7.0 handler:nil];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
 }
 
 - (void)testAcquireToken_whenWPJRequest_shouldReturnWorkplaceJoinRequiredError
