8c8ce04643c3b64125651a39c33a8b032906a9dc
Sergei Demchenko <sedemche@microsoft.com>
2024-10-04 14:17:42 -0700

    Support caller app info for browser SSO flows  (#1427)
    * Provide browser app info for sso ext.
    
    * modified:   changelog.txt
    
    * Update IdentityCore/src/broker_operation/request/browser_native_message_request/MSIDBrowserNativeMessageGetTokenRequest.m
    
    Co-authored-by: Juan Arias <juan-arias@users.noreply.github.com>
    
    * Add tests.
    
    * Fix mac tests.
    
    * Move new api to browser core requests only.
    
    ---------
    
    Co-authored-by: Juan Arias <juan-arias@users.noreply.github.com>

--- IdentityCore/src/broker_operation/request/browser_native_message_request/MSIDBrowserNativeMessageRequest.h
+++ IdentityCore/src/broker_operation/request/browser_native_message_request/MSIDBrowserNativeMessageRequest.h
@@ -33,6 +33,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// Url of the request sender.
 @property (nonatomic) NSURL *sender;
 
+@property (nonatomic, readonly) NSString *localizedApplicationInfo;
+
 @end
 
 NS_ASSUME_NONNULL_END
