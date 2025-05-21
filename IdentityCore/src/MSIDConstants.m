0c1d40c02d2c457ca679ab61cdde127f945dd76d
Juan Arias Roldan <1686668+juan-arias@users.noreply.github.com>
2025-05-20 17:41:25 -0700

    Use different name for SFRT feature flag

--- IdentityCore/src/MSIDConstants.m
+++ IdentityCore/src/MSIDConstants.m
@@ -85,7 +85,7 @@ NSString *const MSID_BROWSER_RESPONSE_SWITCH_BROWSER_RESUME = @"switch_browser_r
 
 NSString *const MSID_FLIGHT_USE_V2_WEB_RESPONSE_FACTORY = @"use_v2_web_response_factory";
 NSString *const MSID_FLIGHT_SUPPORT_DUNA_CBA = @"support_duna_cba_v2";
-NSString *const MSID_FLIGHT_CLIENT_SFRT_STATUS = @"sfrt_status";
+NSString *const MSID_FLIGHT_CLIENT_SFRT_STATUS = @"sfrt_v2";
 
 
 #define METHODANDLINE   [NSString stringWithFormat:@"%s [Line %d]", __PRETTY_FUNCTION__, __LINE__]
