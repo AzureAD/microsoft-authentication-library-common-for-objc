7dd8b978011b85d57c75ff335d0de9fe5b6c4068
josephpab <josephpab@microsoft.com>
2025-06-23 10:40:13 -0700

    Moved NSOption to another class

--- IdentityCore/src/webview/response/MSIDSwitchBrowserResponse.h
+++ IdentityCore/src/webview/response/MSIDSwitchBrowserResponse.h
@@ -27,6 +27,11 @@
 
 @interface MSIDSwitchBrowserResponse : MSIDWebviewResponse
 
+typedef NS_OPTIONS(NSInteger, MSIDSwitchBrowserModes) {
+    BrowserModePrivateSession = 1 << 0,
+    // Add future flags here
+};
+
 @property (nonatomic, readonly) NSString *actionUri;
 @property (nonatomic, readonly) NSString *switchBrowserSessionToken;
 @property (nonatomic, readonly) NSInteger bitMask;
