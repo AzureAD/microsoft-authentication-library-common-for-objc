982bf9acc4cdf182563316b00351f21a66478d52
josephpab <josephpab@microsoft.com>
2025-07-23 10:20:59 -0700

    Added flag

--- IdentityCore/src/webview/response/MSIDSwitchBrowserResponse.m
+++ IdentityCore/src/webview/response/MSIDSwitchBrowserResponse.m
@@ -50,17 +50,20 @@
     {
         if (![self isMyUrl:url redirectUri:redirectUri]) return nil;
         
-        NSError *stateCheckError = nil;
-        BOOL stateValidated = [MSIDSwitchBrowserResponse validateStateParameter:self.parameters[MSID_OAUTH2_STATE]
-                                                                  expectedState:requestState
-                                                                          error:&stateCheckError];
-        if (!stateValidated)
+        if ([MSIDFlightManager.sharedInstance boolForKey:MSID_FLIGHT_SUPPORT_STATE_VALIDATION_DUNA_CBA])
         {
-            if (stateCheckError && error)
+            NSError *stateCheckError = nil;
+            BOOL stateValidated = [MSIDSwitchBrowserResponse validateStateParameter:self.parameters[MSID_OAUTH2_STATE]
+                                                                      expectedState:requestState
+                                                                              error:&stateCheckError];
+            if (!stateValidated)
             {
-                *error = stateCheckError;
+                if (stateCheckError && error)
+                {
+                    *error = stateCheckError;
+                }
+                return nil;
             }
-            return nil;
         }
         
         _actionUri = self.parameters[@"action_uri"];
