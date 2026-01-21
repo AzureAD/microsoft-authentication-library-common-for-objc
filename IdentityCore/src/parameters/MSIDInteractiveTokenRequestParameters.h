0f991da86585edadd84b6c8d7039089ca806f620
Maximus Agubuzo <maximusagubuzo@MacBook-Air-6.local>
2026-01-21 13:42:23 -0600

    update

--- IdentityCore/src/parameters/MSIDInteractiveTokenRequestParameters.h
+++ IdentityCore/src/parameters/MSIDInteractiveTokenRequestParameters.h
@@ -38,7 +38,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic) BOOL enablePkce;
 @property (nonatomic) MSIDBrokerInvocationOptions *brokerInvocationOptions;
 @property (nullable, nonatomic) id<MSIDCustomHeaderProviding> crossDomainHeaderProvider;
-// Additional request parameter that will be utilized by [ ] when doing automation testing. When available will be passed to web view to be consumed by ESTS
+// Additional request parameter that will be utilized by OneAuth during internal automation testing. When available will be passed to web view to be consumed by ESTS as a form of MFA.
 @property (nonatomic, nullable) NSString *userFederatetedIdentityToken;
 
 - (NSOrderedSet *)allAuthorizeRequestScopes;
