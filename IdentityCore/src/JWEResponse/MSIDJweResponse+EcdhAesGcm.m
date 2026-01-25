04e4b3e4dac9eaba002cd336bb5a8e6876992ace
Ameya <amepatil@microsoft.com>
2026-01-24 18:09:26 -0800

    Add custom swift bridge header for cryptokit usage in objc

--- IdentityCore/src/JWEResponse/MSIDJweResponse+EcdhAesGcm.m
+++ IdentityCore/src/JWEResponse/MSIDJweResponse+EcdhAesGcm.m
@@ -31,10 +31,6 @@
 #import "NSData+MSIDEccSecKeyRef.h"
 #import "MSIDEcdhApv.h"
 #import "MSIDJsonSerializer.h"
-#import "IdentityCore_Internal.h"
-#if MSID_IDENTITYCORE_SWIFT_AVAILABLE
-#import "IdentityCore-Swift.h"
-#endif
 #import "MSIDFlightManager.h"
 #import "MSIDConstants.h"
 
@@ -171,7 +167,6 @@ MSIDJWECryptoKeyResponseEncryptionAlgorithm const MSID_RESPONSE_ENCRYPTION_ALGOR
                                          apv:(NSData *)apv
                                        error:(NSError * _Nullable __autoreleasing * _Nullable)error
 {
-#if MSID_IDENTITYCORE_SWIFT_AVAILABLE
     NSError *concatKDFError = nil;
     MSIDConcatKdfProvider *concatKDFProvider = [MSIDConcatKdfProvider new];
     NSData *derivedKey = [concatKDFProvider concatKDFWithSHA256WithSharedSecret:sharedSecret
@@ -194,18 +189,6 @@ MSIDJWECryptoKeyResponseEncryptionAlgorithm const MSID_RESPONSE_ENCRYPTION_ALGOR
     }
     
     return derivedKey;
-#else
-    if (error)
-    {
-        NSDictionary *userInfo = @{ NSLocalizedDescriptionKey : @"Swift cryptographic operations not available" };
-        *error = [NSError errorWithDomain:@"MSIDIdentityCoreErrorDomain"
-                                     code:-1
-                                 userInfo:userInfo];
-    }
-    
-    MSID_LOG_WITH_CTX(MSIDLogLevelError, nil, @"Swift cryptographic operations not available. MSID_IDENTITYCORE_SWIFT_AVAILABLE is disabled, cannot calculate derived key.");
-    return nil;
-#endif
 }
 
 - (NSDictionary *)decryptJweResponseUsingSymmetricKey:(NSData *)symmetricKey
@@ -225,25 +208,11 @@ MSIDJWECryptoKeyResponseEncryptionAlgorithm const MSID_RESPONSE_ENCRYPTION_ALGOR
         return nil;
     }
     NSData *decryptedData;
-#if MSID_IDENTITYCORE_SWIFT_AVAILABLE
+
     // Since only A256GCM is supported, we can decrypt jwe message using AES256GCM.
     MSIDAesGcmDecryptor *decryptor = [MSIDAesGcmDecryptor new];
-    decryptedData = [decryptor decryptWithAES256GCMHandlerWithMessage:self.payload iv:self.iv key:symmetricKey tag:self.tag aad:self.aad error:error];
-#else
-    if (error)
-    {
-        *error = MSIDCreateError(MSIDErrorDomain,
-                                  MSIDErrorInternal,
-                                  @"Swift AES-GCM decryption is unavailable on this platform",
-                                  nil,
-                                  nil,
-                                  nil,
-                                  nil,
-                                  nil,
-                                  YES);
-    }
-    MSID_LOG_WITH_CTX(MSIDLogLevelError, nil, @"Swift AES-GCM decryption is unavailable on this platform");
-#endif
+    decryptedData = [decryptor decryptWithAES256GMCiphertext:self.payload nonce:self.iv key:symmetricKey tag:self.tag aad:self.aad error:error];
+    
     // Deallocate symmetricKey as it is no longer needed
     symmetricKey = nil;
     if (!decryptedData)
