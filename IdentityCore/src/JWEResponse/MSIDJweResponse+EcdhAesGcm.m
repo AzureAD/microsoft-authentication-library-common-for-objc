2793c6dfa3fec06db7963019c5496d42acc18409
Ameya <amepatil@microsoft.com>
2026-01-27 18:35:33 -0800

    Try with bridging generated

--- IdentityCore/src/JWEResponse/MSIDJweResponse+EcdhAesGcm.m
+++ IdentityCore/src/JWEResponse/MSIDJweResponse+EcdhAesGcm.m
@@ -33,9 +33,7 @@
 #import "MSIDJsonSerializer.h"
 #import "MSIDFlightManager.h"
 #import "MSIDConstants.h"
-#ifndef MSAL_SWIFT_H
 #import "MSIDCryptoKitBridge.h"
-#endif
 
 MSIDJWECryptoKeyExchangeAlgorithm const MSID_KEY_EXCHANGE_ALGORITHM_ECDH_ES = @"ECDH-ES";
 MSIDJWECryptoKeyResponseEncryptionAlgorithm const MSID_RESPONSE_ENCRYPTION_ALGORITHM_A256GCM = @"A256GCM";
