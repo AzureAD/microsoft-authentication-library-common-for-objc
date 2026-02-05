c65a873cdeaa5b7f29326e6ef7a947a61659708a
kai <kasong@microsoft.com>
2026-02-05 15:00:34 -0800

    try new scripts

--- IdentityCore/src/telemetry/execution_flow/MSIDExecutionFlowConstants.m
+++ IdentityCore/src/telemetry/execution_flow/MSIDExecutionFlowConstants.m
@@ -77,7 +77,7 @@ NSString *MSIDTokenRequestTagToString(MSIDTokenRequestTag state)
     switch (state)
     {
         case MSIDTokenRequestAtExpirationElapsedTag:
-            return @"jwmta";
+            return @"UNTAGGED";
     }
 
     // Fallback for any future enum values
