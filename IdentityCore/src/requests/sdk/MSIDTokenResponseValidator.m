10430d0f921a214ef9e00367d0015ad0443579f0
kai <kasong@microsoft.com>
2026-03-13 11:47:26 -0700

    Add mapping logic in legacy broker flow on iOS

--- IdentityCore/src/requests/sdk/MSIDTokenResponseValidator.m
+++ IdentityCore/src/requests/sdk/MSIDTokenResponseValidator.m
@@ -257,7 +257,11 @@
     MSID_LOG_WITH_CORR(MSIDLogLevelInfo, correlationID, @"Token result is valid.");
     // Keep old flow for now in case the old MSAL/OneAuth client is broken.
     [tokenResult insertBrokerMetaData:brokerResponse.brokerAppVer forKey:MSID_TOKEN_RESULT_BROKER_APP_VERSION];
-
+    if (brokerResponse.tokenResponse.clientData)
+    {
+        [tokenResult insertBrokerMetaData:brokerResponse.tokenResponse.clientData forKey:MSID_TOKEN_RESULT_CLIENT_DATA];
+    }
+    
     return tokenResult;
 }
 
