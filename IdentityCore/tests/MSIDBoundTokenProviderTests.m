2b2f4d03275cd999ee003d8432dd984c02d4896b
Maximus Agubuzo <maagubuzo@microsoft.com>
2026-06-26 12:11:55 -0500

    Improve MSIDBoundTokenProvider test coverage

--- IdentityCore/tests/MSIDBoundTokenProviderTests.m
+++ IdentityCore/tests/MSIDBoundTokenProviderTests.m
@@ -25,6 +25,7 @@
 #import <XCTest/XCTest.h>
 #import "MSIDBoundTokenProvider.h"
 #import "MSIDBrowserNativeMessageGetTokenRequest.h"
+#import "MSIDError.h"
 
 @interface MSIDBoundTokenProviderTests : XCTestCase
 
@@ -51,6 +52,19 @@
     return request;
 }
 
+- (NSDictionary *)payloadDictionaryFromResponse:(NSString *)response
+{
+    NSData *data = [response dataUsingEncoding:NSUTF8StringEncoding];
+    XCTAssertNotNil(data);
+
+    NSError *jsonError = nil;
+    NSDictionary *payload = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+    XCTAssertNil(jsonError);
+    XCTAssertTrue([payload isKindOfClass:NSDictionary.class]);
+
+    return payload;
+}
+
 // A GetToken request handed to the provider is serviced entirely in-process,
 // returning a payload, with no SSO extension / ASAuthorization involvement.
 - (void)testAcquireBoundToken_inProc_returnsPayload
@@ -63,8 +77,32 @@
                            completionBlock:^(NSString *response, NSError *error) {
         XCTAssertNil(error);
         XCTAssertNotNil(response);
-        XCTAssertTrue([response containsString:@"in_proc_common_core"]);
-        XCTAssertTrue([response containsString:@"MSIDBoundTokenProvider"]);
+
+        NSDictionary *payload = [self payloadDictionaryFromResponse:response];
+        XCTAssertEqualObjects(payload[@"clientId"], @"00000000-0000-0000-0000-000000000001");
+        XCTAssertEqualObjects(payload[@"redirectUri"], @"brk-com.microsoft.test://auth");
+        XCTAssertEqualObjects(payload[@"scope"], @"user.read");
+        XCTAssertEqualObjects(payload[@"state"], @"test-state");
+        XCTAssertEqualObjects(payload[@"transport"], @"in_proc_common_core");
+        XCTAssertEqualObjects(payload[@"servicedBy"], @"MSIDBoundTokenProvider");
+        [expectation fulfill];
+    }];
+
+    [self waitForExpectations:@[expectation] timeout:5.0];
+}
+
+- (void)testAcquireBoundToken_nilRequest_returnsInvalidInternalParameterError
+{
+    MSIDBoundTokenProvider *provider = [MSIDBoundTokenProvider new];
+    MSIDBrowserNativeMessageGetTokenRequest *nilRequest = nil;
+    XCTestExpectation *expectation = [self expectationWithDescription:@"nil request error"];
+
+    [provider acquireBoundTokenWithRequest:nilRequest
+                                   context:nil
+                           completionBlock:^(NSString *response, NSError *error) {
+        XCTAssertNil(response);
+        XCTAssertEqualObjects(error.domain, MSIDErrorDomain);
+        XCTAssertEqual(error.code, MSIDErrorInvalidInternalParameter);
         [expectation fulfill];
     }];
 
@@ -83,7 +121,28 @@
                                    context:nil
                            completionBlock:^(NSString *response, NSError *error) {
         XCTAssertNil(response);
-        XCTAssertNotNil(error);
+        XCTAssertEqualObjects(error.domain, MSIDErrorDomain);
+        XCTAssertEqual(error.code, MSIDErrorInvalidDeveloperParameter);
+        [expectation fulfill];
+    }];
+
+    [self waitForExpectations:@[expectation] timeout:5.0];
+}
+
+- (void)testAcquireBoundToken_missingRedirectUri_returnsInvalidDeveloperParameterError
+{
+    MSIDBoundTokenProvider *provider = [MSIDBoundTokenProvider new];
+    MSIDBrowserNativeMessageGetTokenRequest *request = [self validRequest];
+    request.redirectUri = @"";
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"redirect validation error"];
+
+    [provider acquireBoundTokenWithRequest:request
+                                   context:nil
+                           completionBlock:^(NSString *response, NSError *error) {
+        XCTAssertNil(response);
+        XCTAssertEqualObjects(error.domain, MSIDErrorDomain);
+        XCTAssertEqual(error.code, MSIDErrorInvalidDeveloperParameter);
         [expectation fulfill];
     }];
 
